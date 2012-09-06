5c8d1d2a6f65c89798458308c0b8aff2d6763aa6
Jorin Vogel <jorin.vogel@gmail.com>
2012-09-06 10:52:55 +0200

    update dependencies

--- dev/public/js/main.js
+++ dev/public/js/main.js
@@ -1,9 +1,8 @@
 require.config( {
   paths: {
     text: 'libs/require/text',
-    order: "libs/require/order",
-    jquery: 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min',
-    modernizr: 'libs/modernizr-2.5.3',
+    jquery: ['http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min', 'libs/jquery'],
+    modernizr: 'libs/modernizr',
     underscore: 'libs/underscore.min',
     backbone: 'libs/backbone.min',
     hogan: 'libs/hogan.min'
